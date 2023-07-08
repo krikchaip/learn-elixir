f1b6b7fa0f99c59e9c44667694c4260e458216b9
krikchaip <krikchai.p@gmail.com>
2023-07-09 03:48:17 +0700

    exercism: restructured

--- exercism/collatz_conjecture.ex
+++
@@ -1,7 +0,0 @@
-defmodule CollatzConjecture do
-  def calc(1), do: 0
-  def calc(n) when n > 0 and rem(n, 2) == 1, do: 1 + calc(3*n + 1)
-  def calc(n) when n > 0 and rem(n, 2) == 0, do: 1 + calc(div n, 2)
-end
-
-# CollatzConjecture.calc(0) |> IO.puts
