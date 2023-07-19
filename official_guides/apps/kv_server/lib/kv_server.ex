c75b9c377528ac48f9823a019349f9e29ad13444
krikchaip <krikchai.p@gmail.com>
2023-07-20 02:03:12 +0700

    kv_server subappi init

---
+++ official_guides/apps/kv_server/lib/kv_server.ex
@@ -0,0 +1,24 @@
+defmodule KVServer do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: KVServer.Worker.start_link(arg)
+      # {KVServer.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [
+      name: KVServer.Supervisor,
+      strategy: :one_for_one
+    ]
+
+    Supervisor.start_link(children, opts)
+  end
+end
