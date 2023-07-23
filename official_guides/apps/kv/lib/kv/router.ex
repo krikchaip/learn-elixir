2c5b02332bd51904331a46b422d7b5cc1fedc2cc
krikchaip <krikchai.p@gmail.com>
2023-07-24 01:55:19 +0700

    kv: router implementation

---
+++ official_guides/apps/kv/lib/kv/router.ex
@@ -0,0 +1,43 @@
+defmodule KV.Router do
+  @doc """
+  Dispatch the given `mod`, `fun`, `args` request
+  to the appropriate node based on the `bucket`.
+  """
+  @spec route(String.t(), module(), atom(), list(term())) :: term()
+  def route(bucket, mod, fun, args) do
+    c = String.first(bucket) |> to_charlist() |> hd
+
+    entry =
+      Enum.find(table(), fn {range, _} ->
+        c in range
+      end) || no_entry_error(bucket)
+
+    node_name = elem(entry, 1)
+
+    # If the entry node is the current node, apply as-is
+    if node_name == node() do
+      apply(mod, fun, args)
+    else
+      # Otherwise, apply it on the other node
+      {KV.RouterTasks, node_name}
+      |> Task.Supervisor.async(mod, fun, args)
+      |> Task.await()
+    end
+  end
+
+  # will raise an exception when an entry is out of range.
+  defp no_entry_error(bucket) do
+    raise "could not find entry for #{inspect(bucket)} in table #{inspect(table())}"
+  end
+
+  @doc """
+  The routing table.
+  """
+  def table do
+    # Replace computer-name with your local machine name
+    [
+      {?a..?m, :"a@Krikchais-MacBook-Pro-M1"},
+      {?n..?z, :"b@Krikchais-MacBook-Pro-M1"}
+    ]
+  end
+end
---
+++ official_guides/apps/kv/test/kv/router_test.exs
@@ -0,0 +1,16 @@
+# must be run with `elixir --sname a -S mix test`
+# with node `b` already launched
+defmodule KV.Router.Test do
+  use ExUnit.Case, async: true
+
+  test "route requests across nodes" do
+    assert KV.Router.route("a", Kernel, :node, []) == :"a@Krikchais-MacBook-Pro-M1"
+    assert KV.Router.route("n", Kernel, :node, []) == :"b@Krikchais-MacBook-Pro-M1"
+  end
+
+  test "raises an unknown entries" do
+    assert_raise RuntimeError, ~r/could not find entry/, fn ->
+      KV.Router.route("ก", Kernel, :node, [])
+    end
+  end
+end
