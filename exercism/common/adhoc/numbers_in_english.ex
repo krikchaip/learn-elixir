c513f9ccf3c04818528637730e58ca1c701e382e
krikchaip <krikchai.p@gmail.com>
2024-01-13 18:14:35 +0700

    🧹💨

--- exercism/common/adhoc/numbers_in_english.ex
+++ exercism/common/adhoc/numbers_in_english.ex
@@ -1,15 +1,15 @@
 defmodule Say do
   @dict %{
-    0  => "zero",
-    1  => "one",
-    2  => "two",
-    3  => "three",
-    4  => "four",
-    5  => "five",
-    6  => "six",
-    7  => "seven",
-    8  => "eight",
-    9  => "nine",
+    0 => "zero",
+    1 => "one",
+    2 => "two",
+    3 => "three",
+    4 => "four",
+    5 => "five",
+    6 => "six",
+    7 => "seven",
+    8 => "eight",
+    9 => "nine",
     10 => "ten",
     11 => "eleven",
     12 => "twelve",
@@ -31,8 +31,8 @@ defmodule Say do
   }
 
   @suffixes %{
-    1_000         => "thousand",
-    1_000_000     => "million",
+    1_000 => "thousand",
+    1_000_000 => "million",
     1_000_000_000 => "billion"
   }
 
@@ -42,8 +42,10 @@ defmodule Say do
   @spec in_english(integer) :: {atom, String.t()}
   def in_english(n) when n < 0 or n > 999_999_999_999,
     do: {:error, "number is out of range"}
+
   def in_english(n) when is_map_key(@dict, n),
     do: {:ok, @dict[n]}
+
   def in_english(n) when n < 100 do
     [tens, ones] = Integer.digits(n)
     {:ok, tens} = in_english(tens * 10)
@@ -51,6 +53,7 @@ defmodule Say do
 
     if ones == "zero", do: {:ok, tens}, else: {:ok, "#{tens}-#{ones}"}
   end
+
   def in_english(n) when n < 1_000 do
     [hundreds | tens] = Integer.digits(n)
     {:ok, hundreds} = in_english(hundreds)
@@ -62,12 +65,14 @@ defmodule Say do
       {:ok, "#{hundreds} hundred #{tens}"}
     end
   end
+
   def in_english(n) do
-    digit_groups = n
-      |> Integer.digits
-      |> Enum.reverse
+    digit_groups =
+      n
+      |> Integer.digits()
+      |> Enum.reverse()
       |> Enum.chunk_every(3)
-      |> Enum.reverse
+      |> Enum.reverse()
       |> Enum.map(&Enum.reverse/1)
 
     {_, result} =
@@ -76,8 +81,9 @@ defmodule Say do
           reduce: {digit_groups, ""} do
         {[_ | rest], acc} when group == 0 ->
           {rest, acc}
+
         {[_ | rest], acc} ->
-          remaining_digits = Enum.map(rest, &length/1) |> Enum.sum
+          remaining_digits = Enum.map(rest, &length/1) |> Enum.sum()
           suffix = @suffixes[10 ** remaining_digits]
           {:ok, hundreds} = in_english(group)
           {rest, acc <> " #{hundreds} #{suffix}"}
